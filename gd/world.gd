7bf0eefb53f087011899548377755873c139ccc4
cecilj08-ux <cecil_j08@icloud.com>
2026-05-29 01:49:12 -0500

    Fix stage selection
    Fixed metadata for world 0 gates, added completion check after 0-8 is completed

--- gd/world.gd
+++ gd/world.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _ready() -> void:
-	if get_meta("secret"):
+	if has_meta("secret") and get_meta("secret"):
 		if SaveManager.save_game.unlocked_secrets.get(get_meta("world_id")[0]) == null: SaveManager.save_game.unlocked_secrets[get_meta("world_id")[0]] = []
 		if not get_meta("world_id")[1] in SaveManager.save_game.unlocked_secrets[get_meta("world_id")[0]]:
 			SaveManager.save_game.unlocked_secrets[get_meta("world_id")[0]].append(get_meta("world_id")[1])
@@ -11,3 +11,4 @@ func _ready() -> void:
 		if not get_meta("world_id")[1] in SaveManager.save_game.unlocked_stages[get_meta("world_id")[0]]:
 			SaveManager.save_game.unlocked_stages[get_meta("world_id")[0]].append(get_meta("world_id")[1])
 			SaveManager.save()
+	if has_meta("redirect"): get_tree().change_scene_to_file(get_meta("redirect"))
